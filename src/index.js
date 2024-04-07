b8b150fb734728b8666808ab41118182c9af9f02
Yengzzkie <gatchalian.manuel@gmail.com>
2024-04-07 17:10:22 -0400

    feature/added displayed movies on movie search

--- src/index.js
+++ src/index.js
@@ -12,7 +12,15 @@ const options = {
   },
 };
 
-searchMovieBtn.addEventListener('click', searchMovie)
+searchMovieBtn.addEventListener('click', () => {
+  if (searchMovieInput.value === '') {
+    alert('Please enter movie title')
+  } else {
+    searchMovie();
+  }
+
+  
+})
 
 async function getNowPlayingMovies() { //fetch now playing movies
   try {
@@ -32,25 +40,7 @@ async function getNowPlayingMovies() { //fetch now playing movies
 async function displayNowPlayingMovies() {
   try {
     const nowPlayingMovies = await getNowPlayingMovies();
-    const baseImgURL = "https://image.tmdb.org/t/p/w500/";
-
-    nowPlayingMovies.forEach((movie) => {
-      const movieCard = document.createElement("div");
-      const movieID = document.createElement("button");
-      const movieImage = document.createElement("img");
-      movieID.textContent = movie.title;
-      movieImage.src = `${baseImgURL}${movie.poster_path}`;
-      console.log(movie.title, movie.id, `${baseImgURL}${movie.poster_path}`);
-
-      movieID.addEventListener("click", () => {
-        app.innerHTML = "";
-        console.clear();
-        // displayNowPlayingMovies();
-        watchMovie(movie.title, movie.id, `${baseImgURL}${movie.id}`);
-      });
-      movieCard.append(movieImage, movieID);
-      app.append(movieCard);
-    });
+    displayMovies(nowPlayingMovies);
   } catch (error) {
     console.error(error);
   }
@@ -61,12 +51,13 @@ displayNowPlayingMovies(); //initilize the list of now playing movies on page lo
 async function searchMovie() {
   const searchQuery = searchMovieInput.value;
   try {
-    fetch(
+    const response = await fetch(
       `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&include_adult=false&language=en-US&page=1`,
       options
-    )
-      .then((response) => response.json())
-      .then((response) => console.log(response));
+    );
+    const data = await response.json();
+    const searchResults = data.results;
+    displayMovies(searchResults);
   } catch (error) {
     console.error(error);
   }
@@ -90,6 +81,33 @@ async function watchMovie(movieTitle, movieID) {
     backBtn.addEventListener("click", () => {
       app.innerHTML = "";
       displayNowPlayingMovies();
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function displayMovies(movies) {
+  try {
+    const baseImgURL = "https://image.tmdb.org/t/p/w500/";
+
+    // Clear previous content
+    app.innerHTML = "";
+
+    movies.forEach((movie) => {
+      const movieCard = document.createElement("div");
+      const movieID = document.createElement("button");
+      const movieImage = document.createElement("img");
+      movieID.textContent = movie.title;
+      movieImage.src = `${baseImgURL}${movie.poster_path}`;
+
+      movieID.addEventListener("click", () => {
+        app.innerHTML = "";
+        watchMovie(movie.title, movie.id);
+      });
+
+      movieCard.append(movieImage, movieID);
+      app.append(movieCard);
     });
   } catch (error) {
     console.error(error);
