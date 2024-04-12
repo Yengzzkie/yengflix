c4b3515834364e8451129781e4317aec46d4ed90
Yengzzkie <gatchalian.manuel@gmail.com>
2024-04-12 19:40:31 -0400

    added watch movie function on myList

--- src/modules/addToMyList.js
+++ src/modules/addToMyList.js
@@ -1,3 +1,6 @@
+import watchMovie from "./watchMovie.js";
+import watchSeries from "./watchSeries.js";
+
 export default function addToMyList({ movie, title, type }) {
   // Retrieve existing list from local storage or initialize an empty array
   const myList = JSON.parse(localStorage.getItem("myList")) || [];
@@ -33,7 +36,7 @@ async function displayMyList() {
     const movieContainer = document.createElement("section");
     movieContainer.setAttribute("id", "movie-container");
 
-    myList.forEach((list) => {
+    myList.forEach((movie) => {
       const movieCard = document.createElement("div");
       const btnWrapper = document.createElement("div");
       const watchBtn = document.createElement("button");
@@ -45,20 +48,25 @@ async function displayMyList() {
       const movieRating = document.createElement("p");
 
       movieInfo.setAttribute("id", "movie-info");
-      movieInfo.innerHTML = `<h1>${list.title}</h1>`;
-      movieOverview.textContent = `${list.overview}`;
+      movieInfo.innerHTML = `<h1>${movie.title}</h1>`;
+      movieOverview.textContent = `${movie.overview}`;
       movieRating.innerHTML = `<b>Rating</b> : ${Math.floor(
-        list.vote_average
+        movie.vote_average
       )} / 10`;
-      movieReleaseDate.innerHTML = `<b>Released</b> : ${list.release_date}`;
-      movieImage.src = `${baseImgURL}${list.poster}`;
+      movieReleaseDate.innerHTML = `<b>Released</b> : ${movie.release_date}`;
+      movieImage.src = `${baseImgURL}${movie.poster}`;
       movieCard.setAttribute("id", "movie-card");
       btnWrapper.setAttribute('id', 'button-wrapper');
       watchBtn.innerHTML = `<i class="fa-solid fa-play"></i>`;
       watchBtn.setAttribute("id", "watch-button");
       deleteFromListBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
       deleteFromListBtn.setAttribute("id", "delete-from-list");
-      console.log(list);
+    console.log(myList)
+
+      watchBtn.addEventListener('click', () => {
+        app.innerHTML = '';
+        movie.type === 'movie' ? watchMovie(movie.title, movie.id, movie.backdrop_path) : watchSeries(movie.name, movie.id, movie.backdrop_path);
+      })
 
       movieInfo.append(movieOverview, movieReleaseDate, movieRating);
       btnWrapper.append(watchBtn, deleteFromListBtn);
