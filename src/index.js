85cb6f651949df96a2b48920cdb5f8562c299746
Yengzzkie <gatchalian.manuel@gmail.com>
2024-04-17 15:41:01 -0400

    removed hamburger btn, added search btn to navbar

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const tvSeriesBtn = document.getElementById('tv-series-btn');
 const myListBtn = document.getElementById('my-list');
 const nowPlayingMoviesBtn = document.getElementById('popular-movies-btn');
 const hamburger = document.getElementById('hamburger');
+const search = document.getElementById('search');
 const header = document.querySelector('header');
 const clearField = document.getElementById('clear-field');
 const logo = document.getElementById('mobile-logo')
@@ -41,7 +42,7 @@ myListBtn.addEventListener('click', displayMyList);
 searchSeriesBtn.addEventListener('click', searchSeries); // eventlistener for searching query for TV Series
 
 // HAMBURGER EVENTLISTENER
-hamburger.addEventListener('click', () => { // eventlistener for toggling the hamburger menu
+search.addEventListener('click', () => { // eventlistener for toggling the hamburger menu
   header.classList.toggle('active');
 });
 
