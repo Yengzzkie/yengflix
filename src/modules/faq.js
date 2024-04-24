d1b4ba2e484ab7794313c4a0f5a8b62c1014b7dc
Yengzzkie <gatchalian.manuel@gmail.com>
2024-04-24 18:53:26 -0400

    added new FAQ

--- src/modules/faq.js
+++ src/modules/faq.js
@@ -1,32 +1,45 @@
 const FAQ = [
-    {
-      question: "Is streaming free?",
-      answer:
-        "Oh yeah, it is 100% FREE! However, there are ads on the video every couple of minutes when you try to click on it. You know, developers need to eat too ;). They are completely harmless, just close the ads when they pop up.",
-    },
-    {
-      question: "Why do some videos say Error 404?",
-      answer:
-        "I don't know, it works on my computer sooo... just kidding, some videos are unavailable especially when they are recently released. Come back after few days, weeks, or months; maybe they will get uploaded too. If still unsuccessful, proceed to question #3.",
-    },
-    {
-      question: "I'm searching for a Movie/TV show but it shows 0 results",
-      answer:
-        "Ah, the eternal quest for the perfect binge-watch. If you can't find your beloved show, make sure you are clicking the appropriate button. Click the 'Search Movie' button if you are searching for a movie, and click the 'TV Show' button if you are searching for a TV show. If you are still unsuccessful, go back to question #2.",
-    },
-    {
-      question: "Why does my list disappear when I access it from another device?",
-      answer: "This streaming app uses local storage, which means your lists are stored on the device where you saved them."
-    },
-    {
-      question: "Are there any hidden fees or charges?",
-      answer: "Nope, nada, zilch! My goal is to bring you endless entertainment, not endless bills. So kick back, relax, and let the binge-watching begin!"
-    },
-    {
-      question: "Can I suggest new features or content for the app?",
-      answer: "Absolutely! I love hearing from awesome community. Send me your wildest dreams at gatchalian.manuel@gmail.com, and I'll do my best to turn them into streaming reality."
-    },
-  ];
-  
-  export { FAQ };
-  
+  {
+    question: "Is streaming free?",
+    answer:
+      "Oh yeah, it is 100% FREE! However, there are ads on the video every couple of minutes when you try to click on it. You know, developers need to eat too ;). They are completely harmless, just close the ads when they pop up.",
+  },
+  {
+    question: "Is this site legit?",
+    answer:
+      "Yes! It is more legit than your imaginary girlfriend.",
+  },
+  {
+    question: "Why do some videos say 'Error 404'?",
+    answer:
+      "I don't know, it works on my computer sooo... just kidding, some videos are unavailable especially when they are recently released. Come back after few days, weeks, or months; maybe they will get uploaded too. If still unsuccessful, proceed to question #3.",
+  },
+  {
+    question: "I'm searching for a Movie/TV show but it shows 0 results",
+    answer:
+      "Ah, the eternal quest for the perfect binge-watch. If you can't find your beloved show, make sure you are clicking the appropriate button. Click the 'Search Movie' button if you are searching for a movie, and click the 'TV Show' button if you are searching for a TV show. If you are still unsuccessful, go back to question #2.",
+  },
+  {
+    question: "When do you upload new movies?",
+    answer:
+      "These movies are hosted by a third-party server, so I don't have an ETA for you, I'm just a mere waiter serving the food to your table.",
+  },
+  {
+    question:
+      "Why does my list disappear when I access it from another device?",
+    answer:
+      "This streaming app uses local storage, which means your lists are stored on the device where you saved them.",
+  },
+  {
+    question: "Are there any hidden fees or charges?",
+    answer:
+      "Nope, nada, zilch! My goal is to bring you endless entertainment, not endless bills. So kick back, relax, and let the binge-watching begin!",
+  },
+  {
+    question: "Can I suggest new features or content for the app?",
+    answer:
+      "Absolutely! I love hearing from awesome community. Send me your wildest dreams at gatchalian.manuel@gmail.com, and I'll do my best to turn them into streaming reality.",
+  },
+];
+
+export { FAQ };
