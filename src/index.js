c76f6befe402c0fb58f8895a1d444f63fc1a9449
Yengzzkie <gatchalian.manuel@gmail.com>
2024-04-24 13:09:02 -0400

    added FAQs

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { getNowPlayingMovies } from './modules/getNowPlayingMovies.js';
 import { resetCurrentPage } from './modules/displayCurrentPage.js';
 import displayMyList from './modules/displayMyList.js';
 import intro from './modules/intro.js';
+import { FAQ } from './modules/faq.js';
 
 const searchMovieInput = document.getElementById('search-query');
 const searchMovieBtn = document.getElementById('search-movie-btn');
@@ -15,11 +16,12 @@ const homeBtn = document.getElementById('home-btn');
 const tvSeriesBtn = document.getElementById('tv-series-btn');
 const myListBtn = document.getElementById('my-list');
 const nowPlayingMoviesBtn = document.getElementById('popular-movies-btn');
-const hamburger = document.getElementById('hamburger');
+const faq = document.getElementById('faq');
 const search = document.getElementById('search');
 const header = document.querySelector('header');
 const clearField = document.getElementById('clear-field');
 const logo = document.getElementById('mobile-logo')
+const faqModal = document.getElementById('faq-modal');
 const app = document.getElementById('app');
 
 // NAVIGATION EVENTLISTENERS
@@ -43,8 +45,31 @@ myListBtn.addEventListener('click', displayMyList);
 searchSeriesBtn.addEventListener('click', searchSeries); // eventlistener for searching query for TV Series
 
 // HAMBURGER EVENTLISTENER
-search.addEventListener('click', () => { // eventlistener for toggling the hamburger menu
-  header.classList.toggle('active');
+faq.addEventListener('click', () => {
+  console.log(FAQ)
+  faqModal.innerHTML = '';
+
+  const closeModal = document.createElement('button');
+  closeModal.textContent = 'X';
+  closeModal.addEventListener('click', () => {
+    faqModal.classList.toggle('open');
+    faqModal.close();
+    faqModal.innerHTML = '';
+  });
+
+  faqModal.showModal();
+  faqModal.classList.toggle('open');
+  faqModal.append(closeModal);
+
+  FAQ.forEach((faqs) => {
+    const question = document.createElement('h3');
+    question.textContent = faqs.question;
+
+    const answer = document.createElement('span');
+    answer.textContent = faqs.answer;
+
+    faqModal.append(question, answer);
+  })
 });
 
 // SEARCH QUERY EVENTLISTENERS
@@ -64,6 +89,10 @@ searchMovieBtn.addEventListener('click', () => {
     searchMovie();
     header.classList.toggle('active')
   }
+});
+
+search.addEventListener('click', () => { // eventlistener for toggling the hamburger menu
+  header.classList.toggle('active');
 });
 
 clearField.addEventListener('click', (e) => {
